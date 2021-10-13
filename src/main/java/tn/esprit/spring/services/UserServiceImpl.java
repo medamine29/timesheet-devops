c85631777b913820943fcae6a28994b800ae7d9e
Mohamed Amine <mohamedamine.naffeti2@esprit.tn>
2021-10-13 11:37:18 +0200

    first commit

---
+++ src/main/java/tn/esprit/spring/services/UserServiceImpl.java
@@ -0,0 +1,72 @@
+package tn.esprit.spring.services;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import tn.esprit.spring.entities.User;
+import tn.esprit.spring.repository.UserRepository;
+
+@Service
+public class UserServiceImpl implements IUserService {
+
+	@Autowired
+	UserRepository userRepository;
+
+	private static final Logger l = LogManager.getLogger(UserServiceImpl.class); 
+	
+	@Override
+	public List<User> retrieveAllUsers() { 
+		List<User> users = null; 
+		try {
+	
+			l.info("entering method retrieveAllUsers"); 
+			users = (List<User>) userRepository.findAll();  
+			for (User user : users) {
+			
+			} 
+			l.info("exiting method retrieveAllUsers");  
+		}catch (Exception e) {
+			l.error("error with method retrieveAllUsers : "+e); 
+		}
+
+		return users;
+	}
+
+
+	@Override
+	public User addUser(User u) {
+		// TODO Log à ajouter en début de la méthode 
+		User u_saved = userRepository.save(u); 
+		// TODO Log à ajouter à la fin de la méthode 
+		return u_saved; 
+	}
+
+	@Override 
+	public User updateUser(User u) { 
+		// TODO Log à ajouter en début de la méthode 
+		User u_saved = userRepository.save(u); 
+		// TODO Log à ajouter à la fin de la méthode 
+		return u_saved; 
+	}
+
+	@Override
+	public void deleteUser(String id) {
+		// TODO Log à ajouter en début de la méthode 
+		userRepository.deleteById(Long.parseLong(id)); 
+		// TODO Log à ajouter à la fin de la méthode 
+	}
+
+	@Override
+	public User retrieveUser(String id) {
+		// TODO Log à ajouter en début de la méthode 
+		//User u =  userRepository.findById(Long.parseLong(id)).orElse(null);
+		User u =  userRepository.findById(Long.parseLong(id)).get(); 
+		// TODO Log à ajouter à la fin de la méthode 
+		return u; 
+	}
+
+}
